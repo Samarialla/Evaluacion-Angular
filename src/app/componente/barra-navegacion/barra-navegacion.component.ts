d66e4cb73859160340a3ab1ae832e9dab2219bba
Samarialla <sergojavier23@gmail.com>
2020-02-21 15:29:57 -0300

    se agregar el logout

--- src/app/componente/barra-navegacion/barra-navegacion.component.ts
+++ src/app/componente/barra-navegacion/barra-navegacion.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService } from 'src/app/http.service';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-barra-navegacion',
@@ -10,7 +11,7 @@ import { HttpService } from 'src/app/http.service';
 })
 export class BarraNavegacionComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute, private httpservice: HttpService) { }
+  constructor(private route: ActivatedRoute, private httpservice: HttpService , private authService: AuthenticationService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -18,4 +19,18 @@ export class BarraNavegacionComponent implements OnInit {
   getCantidad() {
     return this.httpservice.total;
   }
+
+  logoutUser() {
+    this.authService.logout()
+      .then(res => {
+        console.log(res);
+        //this.userDetails = undefined;
+        //localStorage.removeItem('user');
+        this.router.navigate(['./']);
+      }, err => {
+        //this.showMessage('danger', err.message);
+      });
+  }
+
+
 }
